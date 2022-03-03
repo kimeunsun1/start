d52a287bf01f3da033c18874b4203cd8ce14c146
user <user@DESKTOP-HMMHVRI>
2022-03-04 02:30:08 +0900

    220303 re

--- start/src/day05/ex/Ex14.java
+++ start/src/day05/ex/Ex14.java
@@ -22,6 +22,52 @@ package day05.ex;
 public class Ex14 {
 
 	public static void main(String[] args) {
+		int a, b, c, d;
+		int result = 0;
+		
+		loop:
+		for(a = 3; a <= 9; a++) {
+			d = a - 2;
+			for(b = 2; b < 10; b++) {
+				
+				if(b == a || b == d) {
+					continue;
+					// 이 경우는 같은 숫자가 발생하는 경우이므로
+					// 조건에 맞지 않으므로 다음 번호를 추출해서
+					// 다른 조건을 비교해야 한다.
+				}
+				
+				// 이 라인을 실행하는 경우는
+				// b가 a, d 와 같은 경우이다.
+				
+				for(c = 1; c < b; c++) {
+//					if(c >= b) {
+						// 이후 회차 포함 이후 회차는 모두 c >= b 경우이니
+						// 조건에 위배되므로 작업을 진행할 필요가 없게 된다.
+//						break;
+//					}
+					
+					if(c == a || c == d) {
+						continue;
+					}
+					
+					// 이 행을 실행하는 순간은 필요한 숫자가 모두 만들어 졌다.
+					// 단지 마지막 조건에 맞는지 검사해주면 된다.
+					int no1 = a * 1000 + b * 100 + c * 10 + d;
+					int no2 = d * 1000 + c * 100 + b * 10 + a;
+					
+					if(no1 + no2 == 16456) {
+						// 이 경우는 모든 조건에 맞는 경우이므로
+						// 우리가 원하는 숫자가 만들어졌다.
+						result = no1;
+						
+						break loop;
+					}
+				}
+			}
+		}
+		System.out.println("찾는 번호는 " + result + " 입니다.");
+		/*
 		int a = 3;
 		int b = 2;
 		int c = 1;
@@ -48,6 +94,7 @@ public class Ex14 {
 				}
 			}
 		}
+		*/
 		/*
 		int a = 1;
 		int b = 1;
